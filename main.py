b711662b38952526b5fbc846047d93af9906360e
SantiagoOchoaQuintero <sochoaqu@unal.edu.co>
2024-02-18 00:15:32 -0500

    Update main.py
    cambiamos las clases para recibir argumentos

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 class Asiento:
-    color = None
-    precio = None
-    registro = None
+    def  __init__(self,color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
 
     def cambiarColor(self, color):
         if(color == "rojo" or color=="verde" or color=="amarillo" or color=="negro" or color=="blanco"):
@@ -9,9 +10,10 @@ class Asiento:
 
 
 class Motor:
-    numeroCilindros = None
-    tipo = None
-    registro = None
+    def __init__(self,numeroCilindros,tipo,registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
 
     def cambioRegistro(self,registro):
         self.registro = registro
@@ -21,12 +23,14 @@ class Motor:
             self.tipo = tipo
 
 class Auto:
-    modelo = None
-    precio = None
-    marca = None
-    asientos = []
-    registro = None
-    cantidadCreados = None
+
+    def __init__(self,modelo,precio,marca,asientos,registro,cantidadCreados):
+        self.modelo = modelo
+        self.precio = precio
+        self.marca = marca
+        self.asientos = []
+        self.registro = registro
+        self.cantidadCreados = cantidadCreados
 
 
     def cantidadAsientos(self):
